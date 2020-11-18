c153868f6f8aa15d349a4318095ba95432d6c37a
Ryan Peek <rapeek@ucdavis.edu>
2020-11-18 15:12:00 -0800

    delete old files

--- R/f_iterate_ffc.R
+++ R/f_iterate_ffc.R
@@ -6,26 +6,30 @@ library(fs)
 library(here)
 
 # write a function to pull the data
-ffc_iter <- function(id, startDate, ffctoken=ffctoken, save=TRUE){
-    # set special parameters for this run of the FFC
-    ffc <- FFCProcessor$new()
-    ffc$gage_start_date = startDate
-    ffc$warn_years_data = 12
-    ffc$fail_years_data = 10
-    # run the FFCProcessor's setup code, then run the FFC itself
-    ffc$set_up(gage_id = id, token=ffctoken)
-    ffc$run()
+ffc_iter <- function(id, startDate, ffctoken=ffctoken, dirToSave="output/ffc", save=TRUE){
 
-    if(save==TRUE){
-      dir_create(glue("{here()}/output/ffc"))
-      # write out
-      write_csv(ffc$alteration, file = glue::glue("{here()}/output/ffc/{id}_alteration.csv"))
-      write_csv(ffc$ffc_results, file = glue::glue("{here()}/output/ffc/{id}_ffc_results.csv"))
-      write_csv(ffc$ffc_percentiles, file=glue::glue("{here()}/output/ffc/{id}_ffc_percentiles.csv"))
-      write_csv(ffc$predicted_percentiles, file=glue::glue("{here()}/output/ffc/{id}_predicted_percentiles.csv"))
-      } else {
-        return(ffc)
-        }
+  # set save dir
+  outDir <- glue::glue("{here()}/{dirToSave}")
+
+  # set special parameters for this run of the FFC
+  ffc <- FFCProcessor$new()
+  ffc$gage_start_date = startDate
+  ffc$warn_years_data = 12
+  ffc$fail_years_data = 10
+  # run the FFCProcessor's setup code, then run the FFC itself
+  ffc$set_up(gage_id = id, token=ffctoken)
+  capture.output(ffc$run(), file = glue::glue("{outDir}/{id}_logfile.txt"), append = TRUE)
+
+  if(save==TRUE){
+    dir_create(glue("{outDir}"))
+    # write out
+    write_csv(ffc$alteration, file = glue::glue("{outDir}/{id}_alteration.csv"))
+    write_csv(ffc$ffc_results, file = glue::glue("{{outDir}/{id}_ffc_results.csv"))
+    write_csv(ffc$ffc_percentiles, file=glue::glue("{outDir}/{id}_ffc_percentiles.csv"))
+    write_csv(ffc$predicted_percentiles, file=glue::glue("{outDir}/{id}_predicted_percentiles.csv"))
+  } else {
+    return(ffc)
+  }
 }
 
 # wrap in possibly to permit error catching
