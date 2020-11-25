6ffa10cf7dcdc5495ac1fea457459a0e61aa835c
Ryan Peek <rapeek@ucdavis.edu>
2020-11-25 08:52:39 -0800

    organize and rename

--- R/version_comparison_workflow.R
+++ R/version_comparison_workflow.R
@@ -7,7 +7,7 @@ source("R/version_comparison_packages.R")
 # 1. Unzip Data -----------------------------------------------------------
 
 # function to unzip folders
-source("R/data_unzip.R")
+source("R/f_data_unzip.R")
 
 data_unzip(ffc_zip = 2019)
 data_unzip(ffc_zip = 2020)
@@ -16,13 +16,13 @@ data_unzip(ffc_zip = 2020)
 
 # collapses all individual csvs from each stream class into single file
 # retains pertinent stream class/version info
-source("R/data_raw_combine.R")
+source("R/f_data_raw_combine.R")
 v2020 <- data_raw_combine(ffc_version = 2020)
 v2019 <- data_raw_combine(ffc_version = 2019)
 
 # 3. Merge both versions into single file (and tidy) ----------------------
 
-source("R/data_merge.R")
+source("R/f_data_merge.R")
 
 df_merge <- data_merge(ffc_v1 = "2019", ffc_v2 = "2020")
 
@@ -30,7 +30,7 @@ df_merge <- data_merge(ffc_v1 = "2019", ffc_v2 = "2020")
 
 # can specify by stream class and flow characteristics
 # "Timing"         "Magnitude"      "Duration"       "Rate of change" "Frequency"
-source("R/data_plot_summarize.R")
+source("R/f_data_plot_summarize.R")
 
 # view by stream class and flow component/characteristic
 plot_flow_chx(data=df_merge, stream_class = "Class-1", flow_characteristic = "Magnitude", save = TRUE)
